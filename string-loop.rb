36d2dd00b9c5afd132fabc3a830f162a672d249c
Obiamaka Agbaneje <obiamaka.agbaneje@gmail.com>
2016-02-16 13:48:16 +0100

    Number the characters

--- string-loop.rb
+++ string-loop.rb
@@ -1,7 +1,13 @@
+=begin
+This program prints out the characters
+of a string.
+=end
 print "Input string: "
 text = gets.chomp
 text.upcase!
 characters = text.split("")
+i = 0
 characters.each do |character|
-	puts character
+	i += 1
+	puts "#{i} #{character}"
 end
